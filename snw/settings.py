9662d6d244f7b484cc6aa2b19cb28e6251f156cb
Reece Mathews <mathewsreece@gmail.com>
2023-05-10 22:07:12 -0500

    fix key path

--- snw/settings.py
+++ snw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("django_key.txt") as f:
+with open(BASE_DIR / "django_key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
